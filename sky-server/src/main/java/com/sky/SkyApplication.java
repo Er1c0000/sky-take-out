8aa7c6d9e6980ff15cc3ebe6759784445248d393
Er1c0000 <1403011472@qq.com>
2025-07-09 17:02:08 +0800

    新增菜品业务代码开发

--- sky-server/src/main/java/com/sky/SkyApplication.java
+++ sky-server/src/main/java/com/sky/SkyApplication.java
@@ -1,16 +1,25 @@
 package com.sky;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.annotation.PostConstruct;
 
 @SpringBootApplication
 @EnableTransactionManagement //开启注解方式的事务管理
 @Slf4j
 public class SkyApplication {
+    @Autowired
+    private ApplicationContext applicationContext;
+
     public static void main(String[] args) {
         SpringApplication.run(SkyApplication.class, args);
         log.info("server started");
+
     }
+
 }
