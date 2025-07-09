02599f7d6ab6a34471531d62ef4a239648e630f6
Er1c0000 <1403011472@qq.com>
2025-07-09 11:05:42 +0800

    公共字段代码填充

--- sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
+++ sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
@@ -1,12 +1,15 @@
 package com.sky.mapper;
 
 import com.github.pagehelper.Page;
+import com.sky.annotation.AutoFill;
 import com.sky.dto.EmployeePageQueryDTO;
 import com.sky.entity.Employee;
+import com.sky.enumeration.OperationType;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @Mapper
 public interface EmployeeMapper {
@@ -25,6 +28,7 @@ public interface EmployeeMapper {
      * @param employee
      * @return
      */
+    @AutoFill(value = OperationType.INSERT)
     @Insert("insert into employee (name, username, password, phone, sex, id_number, create_time, update_time, create_user, update_user) " +
             "values (#{name}, #{username}, #{password}, #{phone}, #{sex}, #{idNumber}, #{createTime}, #{updateTime}, #{createUser}, #{updateUser}) ")
     void insert(Employee employee);
@@ -41,6 +45,7 @@ public interface EmployeeMapper {
      * @param employee
      * @return
      */
+    @AutoFill(value = OperationType.UPDATE)
     void update(Employee employee);
 
     /**
